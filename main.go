2da58dd99e254dd2f3a735c58e3aa11a108a7322
FrancisVace <lukecamer@gmail.com>
2024-02-24 00:34:16 +1000

    Add test json response

--- main.go
+++ main.go
@@ -113,7 +113,27 @@ func newApp(ctx context.Context, port, projectID string) (*App, error) {
 
 	router := gin.Default()
 	router.GET("/", app.HandlerGin)
+	router.GET("/albums", getAlbums)
 	app.Server.Handler = router
 
 	return app, nil
+}
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
